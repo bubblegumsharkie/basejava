08a5b16ed521c148f6f083aff74e81d5ab1726b7
Alex <kozlov.alex.dev@gmail.com>
2022-09-15 22:03:40 +0300

    ❔HW4: Optional Task for Reflections API

---
+++ src/main/java/org/resumebase/MainReflection.java
@@ -0,0 +1,18 @@
+package org.resumebase;
+
+import org.resumebase.model.Resume;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+
+public class MainReflection {
+    public static void main(String[] args) throws IllegalAccessException, InvocationTargetException {
+        Resume r = new Resume();
+        Field declaredField = r.getClass().getDeclaredFields()[0];
+        declaredField.setAccessible(true);
+        System.out.println(Arrays.toString(r.getClass().getDeclaredMethods()));
+        System.out.println(r.getClass().getDeclaredMethods()[2].invoke(r));
+
+    }
+}
