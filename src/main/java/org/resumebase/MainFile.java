2f30bdb32a246b46fce61aa7a622c73966308f6f
Alex <kozlov.alex.dev@gmail.com>
2022-11-01 09:43:40 +0300

    HW8: Lesson about Date & Files

---
+++ src/main/java/org/resumebase/MainFile.java
@@ -0,0 +1,19 @@
+package org.resumebase;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+public class MainFile {
+
+    public static void main(String[] args) {
+        File file = new File("./.gitignore");
+
+        try(InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(file))) {
+            System.out.println(inputStreamReader.read());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("that's it");
+    }
+}
