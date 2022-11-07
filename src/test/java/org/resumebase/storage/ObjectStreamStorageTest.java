1680bbe050ec5091d22451415ad28c48c10759df
Alex <kozlov.alex.dev@gmail.com>
2022-11-07 23:20:14 +0300

    trying something with github actions

--- src/test/java/org/resumebase/storage/ObjectStreamStorageTest.java
+++ src/test/java/org/resumebase/storage/ObjectStreamStorageTest.java
@@ -1,7 +1,5 @@
 package org.resumebase.storage;
 
-import org.junit.jupiter.api.BeforeEach;
-
 class ObjectStreamStorageTest extends AbstractStorageTest {
     public ObjectStreamStorageTest() {
         super(new ObjectStreamStorage(AbstractStorageTest.STORAGE_DIR));
