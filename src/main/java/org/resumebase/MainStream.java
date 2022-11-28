c7cdb52fd903a3f29d74c3f3a3fa682c8005a7d8
Alex <kozlov.alex.dev@gmail.com>
2022-11-28 15:06:09 +0300

    Stream API tasks

---
+++ src/main/java/org/resumebase/MainStream.java
@@ -0,0 +1,64 @@
+package org.resumebase;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class MainStream {
+
+    /*
+
+    Реализовать метод через стрим int minValue(int[] values).
+    Метод принимает массив цифр от 1 до 9, надо выбрать уникальные
+    и вернуть минимально возможное число, составленное из этих уникальных цифр.
+    Не использовать преобразование в строку и обратно.
+    Например {1,2,3,3,2,3} вернет 123, а {9,8} вернет 89
+
+    Реализовать метод List<Integer> oddOrEven(List<Integer> integers).
+    Если сумма всех чисел нечетная — удалить все нечетные, если четная — удалить все четные.
+    Сложность алгоритма должна быть O(N).
+    Optional - решение в один стрим.
+
+    */
+
+    public static void main(String[] args) {
+
+        System.out.println(minValue(new int[]{1, 3, 2, 3, 2, 3}));
+        System.out.println(minValue(new int[]{9, 8}));
+
+        System.out.println(oddOrEven(Arrays.asList(1, 2, 3, 4, 5)));
+        System.out.println(oddOrEven(Arrays.asList(1, 2, 3, 4, 5, 6, 7)));
+    }
+
+    private static int minValue(int[] values) {
+        return IntStream
+                .range(0, (int) Arrays.stream(values).distinct().count())
+                .boxed()
+                .sorted(Comparator.reverseOrder())
+                .map(index -> (
+                                (Math.pow(10, index)) * (Integer) Arrays.stream(values)
+                                        .boxed()
+                                        .sorted(Comparator.reverseOrder())
+                                        .distinct()
+                                        .toArray()[index]
+                        )
+                )
+                .mapToInt(Double::intValue)
+                .sum();
+    }
+
+    private static List<Integer> oddOrEven(List<Integer> integers) {
+        return integers.stream()
+                .filter(
+                        integer ->
+                                integer % 2 != (
+                                        integers.stream()
+                                                .mapToInt(Integer::intValue)
+                                                .sum() % 2
+                                )
+                ).collect(Collectors.toList());
+    }
+
+}
