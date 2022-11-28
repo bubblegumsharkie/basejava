697e0f12385af1f2365a56e5d4fdbe77fecfdb47
Alex <kozlov.alex.dev@gmail.com>
2022-11-28 15:05:54 +0300

    Concurrency lesson

--- src/main/java/org/resumebase/MainConcurrency.java
+++ src/main/java/org/resumebase/MainConcurrency.java
@@ -1,13 +1,76 @@
 package org.resumebase;
 
-public class MainConcurrency {
-    private static int counter;
+import java.util.concurrent.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-    public static void main(String[] args) {
+public class MainConcurrency {
+    public static final int THREADS_NUMBER = 10000;
+    private static final Object LOCK = new Object();
+    private static final Lock lock1 = new ReentrantLock();
+    private int counter;
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(Thread.currentThread().getName());
+
+        Thread thread0 = new Thread() {
+            @Override
+            public void run() {
+                System.out.println(getName() + ", " + getState());
+                throw new IllegalStateException();
+            }
+        };
+        thread0.start();
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + ", " + Thread.currentThread().getState());
+            }
+
+            private void inc() {
+                synchronized (this) {
+//                    counter++;
+                }
+            }
+
+        }).start();
+
+        System.out.println(thread0.getState());
+
+        final MainConcurrency mainConcurrency = new MainConcurrency();
+//        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
+        CountDownLatch latch = new CountDownLatch(THREADS_NUMBER);
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        for (int i = 0; i < THREADS_NUMBER; i++) {
+            Thread thread = new Thread(() -> {
+                for (int j = 0; j < 100; j++) {
+                    mainConcurrency.inc();
+                }
+                latch.countDown();
+            });
+            thread.start();
+//            threads.add(thread);
+        }
+//        threads.forEach(t -> {
+//            try {
+//                t.join();
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        });
+
+        if (!latch.await(10, TimeUnit.SECONDS)) {
+            System.out.println("TIMEOUT");
+        }
+
+        System.out.println(mainConcurrency.counter);
 
     }
 
-    private static synchronized void inc() {
+    private synchronized void inc() {
         counter++;
     }
 
