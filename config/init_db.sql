508cdfd3602944d186ba1a6b422420979b43487a
Alex <kozlov.alex.dev@gmail.com>
2022-12-01 14:55:51 +0300

    init of DB script added

---
+++ config/init_db.sql
@@ -0,0 +1,16 @@
+CREATE TABLE resume
+(
+    uuid      CHARACTER(36) PRIMARY KEY NOT NULL,
+    full_name TEXT                      NOT NULL
+);
+
+CREATE TABLE contact
+(
+    id          SERIAL        NOT NULL,
+    resume_uuid CHARACTER(36) NOT NULL REFERENCES resume (uuid) ON DELETE CASCADE,
+    type        TEXT          NOT NULL,
+    value       TEXT          NOT NULL
+);
+
+CREATE UNIQUE INDEX contact_uuid_type_index ON contact (resume_uuid, type);
+
