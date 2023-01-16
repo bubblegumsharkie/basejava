6ca07c35104d6987599964123f05d17da25b2d96
Alex <kozlov.alex.dev@gmail.com>
2023-01-16 10:08:53 +0300

    init_db.sql now creates SECTION table

--- config/init_db.sql
+++ config/init_db.sql
@@ -12,5 +12,14 @@ CREATE TABLE contact
     value       TEXT          NOT NULL
 );
 
+CREATE TABLE section
+(
+    id          SERIAL        NOT NULL,
+    resume_uuid CHARACTER(36) NOT NULL REFERENCES resume (uuid) ON DELETE CASCADE,
+    type        TEXT          NOT NULL,
+    value       TEXT          NOT NULL
+);
+
 CREATE UNIQUE INDEX contact_uuid_type_index ON contact (resume_uuid, type);
+CREATE UNIQUE INDEX section_uuid_type_index ON section (resume_uuid, type);
 
