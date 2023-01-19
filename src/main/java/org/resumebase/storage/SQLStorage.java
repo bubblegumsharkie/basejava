cdcff9114ca577ffe9fe947922acc08b2e3bd237
Alex <kozlov.alex.dev@gmail.com>
2023-01-19 10:55:44 +0300

    Including the driver via Class.forName(), refactoring the get() method as well as implementing switch-case for Sections

--- src/main/java/org/resumebase/storage/SQLStorage.java
+++ src/main/java/org/resumebase/storage/SQLStorage.java
@@ -10,11 +10,15 @@ import java.util.List;
 import java.util.Map;
 
 public class SQLStorage implements Storage {
+    private static final String STRING_SPLITTER = "/n";
     private final SQLHelper sqlHelper;
 
-    private static final String STRING_SPLITTER = "/n";
-
     public SQLStorage(String dbUrl, String dbUser, String dbPassword) {
+        try {
+            Class.forName("org.postgresql.Driver").getDeclaredConstructor().newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         sqlHelper = new SQLHelper(() -> DriverManager.getConnection(dbUrl, dbUser, dbPassword));
     }
 
@@ -49,6 +53,8 @@ public class SQLStorage implements Storage {
                     "SELECT * FROM resume r" +
                     " LEFT JOIN contact c " +
                     "  ON r.uuid = c.resume_uuid " +
+                    " LEFT JOIN section s " +
+                    "  ON r.uuid = s.resume_uuid " +
                     " WHERE r.uuid =?;")) {
                 preparedStatement.setString(1, uuid);
                 ResultSet resultSet = preparedStatement.executeQuery();
@@ -58,20 +64,6 @@ public class SQLStorage implements Storage {
                 resume = new Resume(uuid, resultSet.getString("full_name"));
                 do {
                     addContactsToResume(resume, resultSet);
-                } while (resultSet.next());
-            }
-
-            try (PreparedStatement preparedStatement = connection.prepareStatement("" +
-                    "SELECT * FROM resume r" +
-                    " LEFT JOIN section s " +
-                    "  ON r.uuid = s.resume_uuid " +
-                    " WHERE r.uuid =?;")) {
-                preparedStatement.setString(1, uuid);
-                ResultSet resultSet = preparedStatement.executeQuery();
-                if (!resultSet.next()) {
-                    throw new NotExistStorageException(uuid);
-                }
-                do {
                     addSectionsToResume(resume, resultSet);
                 } while (resultSet.next());
             }
@@ -161,24 +153,27 @@ public class SQLStorage implements Storage {
     }
 
     private void addSectionsToResume(Resume resume, ResultSet resultSetSections) throws SQLException {
-        String value = resultSetSections.getString("value");
+        String value = resultSetSections.getString("section_value");
         List<String> values;
         if (value != null) {
-            SectionType sectionType = SectionType.valueOf(resultSetSections.getString("type"));
-            switch (sectionType) {
-                case PERSONAL:
-                case OBJECTIVE:
-                    resume.addSection(sectionType, new TextSection(value));
-                    break;
-                case ACHIEVEMENT:
-                case QUALIFICATIONS:
-                    values = List.of(value.split(STRING_SPLITTER));
-                    resume.addSection(sectionType, new ListSection(values));
-                    break;
-                case EDUCATION:
-                case EXPERIENCE:
-                    // TODO: Implement OrganizationSection here
-                    break;
+            try {
+                SectionType sectionType = SectionType.valueOf(resultSetSections.getString("section_type"));
+                switch (sectionType) {
+                    case PERSONAL:
+                    case OBJECTIVE:
+                        resume.addSection(sectionType, new TextSection(value));
+                        break;
+                    case ACHIEVEMENT:
+                    case QUALIFICATIONS:
+                        values = List.of(value.split(STRING_SPLITTER));
+                        resume.addSection(sectionType, new ListSection(values));
+                        break;
+                    case EDUCATION:
+                    case EXPERIENCE:
+                        // TODO: Implement OrganizationSection here
+                        break;
+                }
+            } catch (IllegalArgumentException e) {
             }
         }
 
@@ -200,29 +195,31 @@ public class SQLStorage implements Storage {
 
     private void insertSectionsToResume(Resume resume, Connection connection) throws SQLException {
         try (PreparedStatement preparedStatement = connection.prepareStatement(("" +
-                "INSERT INTO section (resume_uuid, type, value) VALUES (?,?,?);"))) {
+                "INSERT INTO section (resume_uuid, section_type, section_value) VALUES (?,?,?);"))) {
             for (Map.Entry<SectionType, AbstractSection> section : resume.getSections().entrySet()) {
                 AbstractSection sectionValue = section.getValue();
-                StringBuilder stringBuilder = new StringBuilder();
+                SectionType sectionType = section.getKey();
+                String resultString = "";
 
-                if (sectionValue instanceof ListSection) {
-                    ListSection value = (ListSection) sectionValue;
-                    List<String> items = value.getItems();
-                    for (String item : items) {
-                        stringBuilder.append(item).append(STRING_SPLITTER);
-                    }
+                switch (sectionType) {
+                    case PERSONAL:
+                    case OBJECTIVE:
+                        resultString = ((TextSection) sectionValue).getText();
+                        break;
+                    case ACHIEVEMENT:
+                    case QUALIFICATIONS:
+                        ListSection value = (ListSection) sectionValue;
+                        List<String> items = value.getItems();
+                        resultString = String.join(STRING_SPLITTER, items);
+                        break;
+                    case EXPERIENCE:
+                    case EDUCATION:
+                        // TODO: Implement OrganizationSection here
+                        break;
                 }
-                if (sectionValue instanceof TextSection) {
-                    stringBuilder.append(((TextSection) sectionValue).getText());
-                }
-
-//                if (sectionValue instanceof OrganizationSection) {
-                // TODO: Implement OrganizationSection here
-//                }
-
                 preparedStatement.setString(1, resume.getUuid());
                 preparedStatement.setString(2, section.getKey().name());
-                preparedStatement.setString(3, stringBuilder.toString());
+                preparedStatement.setString(3, resultString);
                 preparedStatement.addBatch();
             }
             preparedStatement.executeBatch();
